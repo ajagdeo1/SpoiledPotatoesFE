11b8195d6c34992910d4574bed58d60e86038b39
Adrian <a.jagdeo19@gmail.com>
2023-09-10 19:33:11 -0400

    front end completed, screen size changes dynamically, trailers work and displays the background posters, reviews can be submitted and added to database, one issue where the client isnt displaying the list of previously submitted reviews, might look into that at another time.

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL:'https://a5ec-108-30-195-102.ngrok-free.app',
+    baseURL: 'https://cdfa-108-30-195-102.ngrok-free.app',
     headers: {"ngrok-skip-browser-warning": true}
 });
